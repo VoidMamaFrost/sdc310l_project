6be5a2b4d27b7bd19d31988039014eace3de3dd4
VoidMamaFrost <92021369+VoidMamaFrost@users.noreply.github.com>
2025-08-31 16:43:27 -0400

    Add files via upload

---
+++ catalog.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>SDC310L Project - BenLev2833</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <h2>Products on offer:</h2>
+        <table>
+            <tr style="font-size:large;">
+                <th>Product #</th>
+                <th>Product Name</th>
+                <th>Product Description</th>
+                <th>Cost(in pennies)</th>
+            </tr>
+        </table>
+    </body>
+</html>
